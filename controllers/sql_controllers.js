a5e191911895d1299d321511abc89e5f9ee9446a
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-03-28 19:01:39 +0200

    Created regex validation for signup formulary

--- controllers/sql_controllers.js
+++ controllers/sql_controllers.js
@@ -1,18 +1,21 @@
 const { createUser } = require('../utils/sql_functions');
+const { signUpValidations } = require('../utils/formulary_validations');
 
 const signUp = async (req, res) => {
     const data = {
         name: req.body['signup-name'],
         email: req.body['signup-email'],
-        password: req.body['signup-password']
+        password: req.body['signup-password'],
+        password2: req.body['signup-password2']
     }
     try {
+        await signUpValidations(data);
         const newUser = await createUser(data);
-        console.log(newUser)
-        res.end();
+        res.send(newUser);
     }
     catch(error) {
         console.log(error);
+        res.end();
     }
 }
 
---
+++ utils/formulary_validations.js
@@ -0,0 +1,23 @@
+const signUpValidations = async (data) => {
+    let error = [];
+    const name = data.name.match(/^[\w\.\s]{0,20}$/gi);
+    if (!name) {
+        error.push('name');
+    }
+    const email = data.email.match(/^[\w\.\-]+@[\w\.\-]+\.[\w]{1,4}$/gi);
+    if (!email) {
+        error.push('email')
+    }
+    const password = data.password.match(/^[\w!@#$%€&/()=?¿¡*+;,:.\-_<>\\]{6,50}$/gi);
+    const password2 = data.password2;
+    if (!password || password.join('') !== password2) {
+        error.push('password')
+    }
+    if (error.length > 0) {
+        throw `Incorrect ${error}.`
+    }
+}
+
+module.exports = {
+    signUpValidations
+}
