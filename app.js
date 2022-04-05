1336c551de2a4d7214b431f224abf7d105bd4243
JoseRodriM <josearm_1601@hotmail.com>
2022-04-05 16:13:09 +0200

    creación del CRUD mongob

--- app.js
+++ app.js
@@ -1,40 +1,24 @@
 const apiKey = process.env.API_KEY;
 const express = require('express');
 const mongoose = require('mongoose');
-
-const Movie = require("./models/Movies")
-
 const app = express()
 const port = 3000
-
 app.set('view engine', 'pug');
 app.set('views', './views');
-
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get("/createMovie", (req,res)=>{
     res.render("createMovie")
 })
-
-app.post("/createMovie", (req,res)=>{
-    let mov = new Movie(req.body);
-    mov.save()
-    .then(item =>{
-        res.send("item saved to database");
-    })
-    .catch(err =>{
-        res.status(400).send(err);
-    })
-})
-
-app.use("/editMovie/", (req, res)=>{
-    
-})
-
-app.use("/removeMovie", (req, res) =>{
-    res.render('removeMovie')
-})
-
+const {createMovie, getMovies, getMovie, updateMovie, getMoviesDel, deleteMovie, getMovieDel} = require("./routes/movies.js")
+app.post("/createMovie", createMovie)
+app.get("/editMovie", getMovies)
+app.get("/editMovie/:titulo", getMovie)
+app.post("/editMovie/:titulo", updateMovie)
+//app.get("/removeMovie", getMoviesDel)
+app.get("/removeMovie/:titulo", getMovieDel)
+app.post("/removeMovie/:titulo", deleteMovie)
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
--- models/Movies.js
+++ models/Movies.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const Schema = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/movapp");
 const movieSchema = {
   titulo:String,
