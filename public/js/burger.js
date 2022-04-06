128c80c2d86b303127fc1429e39bc1efe2b4d21a
Alexdelaoc <alexdelaoc@gmail.com>
2022-04-06 18:48:03 +0200

    added navbar

---
+++ public/js/burger.js
@@ -0,0 +1,11 @@
+const burguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav__menu");
+burguer.addEventListener("click", () => {
+    burguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click", () => {
+    burguer.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
