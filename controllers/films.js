b816954ab779106046dd46302a51366a629fd2c9
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-04-07 19:53:41 +0200

    Fixed bug: unable to display movies when MongoDB sends one, but OMDB doesn't. Since OMDB response it's undefined, pug cannot read properties of undefined, and it starts reading the response because there actually is a response --just from MongoDB--, so the filter <<if not response>> doesn't work here. Added a new filter in backend to check OMDB response before sending it

--- controllers/films.js
+++ controllers/films.js
@@ -20,11 +20,16 @@ const getListOfFilms = async (req, res) => {
             let response = await getFilmsByTitle(movie);
             let mongoResponse = await Movie.find({ titulo: movie });
 
-            if (mongoResponse.length > 0) {
+            if (mongoResponse.length > 0 && response) {
                 response = mongoResponse.concat(response)
+                console.log('From loop')
+            }
+            else if (mongoResponse.length > 0) {
+                response = mongoResponse;
             }
             const user = await findUserById(req.user.user_id);
             const favourites = user.favourites;
+            console.log(response)
             res.status(200).render("searchView", { response, favourites })
         } catch (err) {
             res.status(400).json({ message: err })
