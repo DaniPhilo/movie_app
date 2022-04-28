e87b0397a2721dd8309ba79d665674b453444472
DaniPhilo <daniel.carrasco.casado@gmail.com>
2022-04-28 12:02:56 +0200

    Fixed google callback url to match heroku deployment

--- utils/passport_google_auth.js
+++ utils/passport_google_auth.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
     clientID: '986707039858-kp1u8eubk0glerup7lk8q2jp9ocmrcfo.apps.googleusercontent.com',
     clientSecret: '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',
-    callbackURL: 'http://localhost:3000/auth/google/callback',
+    callbackURL: '/auth/google/callback',
     passReqToCallback: true
 },
     async function (request, accesToken, refreshToken, profile, done) {
